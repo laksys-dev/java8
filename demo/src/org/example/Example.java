8fd386a4ee5dd65dbf04c69a5b7a343c5597b443
Lakshmanan.0018 <Lakshmanan.0018@ML49.mnw.local>
2022-03-22 16:44:06 +0530

    initial commit

---
+++ demo/src/org/example/Example.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to wonderful world of computes!");
+	}
+
+}
