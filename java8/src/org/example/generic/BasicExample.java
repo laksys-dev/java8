c08f087b24ad1eb9071c3381c926c2492270a36b
Lakshmanan.0018 <Lakshmanan.0018@ML49.mnw.local>
2022-03-22 15:25:32 +0530

    initial commit

---
+++ java8/src/org/example/generic/BasicExample.java
@@ -0,0 +1,10 @@
+package org.example.generic;
+
+public class BasicExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, generics");
+	}
+
+}
